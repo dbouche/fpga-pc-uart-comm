84e0486906ff9057f61522f2849a0cfa02fc9329
dbouche <62671119+dbouche@users.noreply.github.com>
2025-05-14 16:37:17 -0500

    Update uart_tx.v
    added a FSM to the Tx logic and fixed a "truncated variable" warning by adding or substracting the exact amount of bits (i.e., + 1'b1)

--- src/uart_tx.v
+++ src/uart_tx.v
@@ -1,6 +1,7 @@
+//Tx
 module uart_tx #(parameter  BAUDRATE = 115200, 
                             CLOCK_FREQ = 27000000,
-                            BAUD_TICKS = CLOCK_FREQ / BAUDRATE)
+                            BAUD_TICKS = CLOCK_FREQ / BAUDRATE) 
 (
     input clock,          // clk input
     input n_reset,        // reset input
@@ -13,7 +14,13 @@ module uart_tx #(parameter  BAUDRATE = 115200,
 // Tx variables
 reg [15:0] baud_counter; 
 reg [3:0] bit_idx;
-reg [9:0] tx_shift; // start + data + stop bits
+reg [9:0] tx_shift; // [9] = stop bit, [8:1] = data[0:7], [0] = start bit
+
+// State machine
+reg [1:0] state;
+localparam  IDLE = 2'b00,
+            SEND = 2'b01,
+            DONE = 2'b10;
 
 always @(posedge clock or negedge n_reset)
 begin
@@ -24,39 +31,48 @@ begin
         baud_counter <= 0;
         bit_idx <= 0;
         tx_shift <= 10'b1111111111;
+        state <= IDLE;
       end
     else
       begin
-        if (!tx_busy) 
-          begin
-            if (tx_start) 
+        case(state)
+            IDLE:
               begin
-                // Load start bit, data bits, and stop bit(s)
-                tx_shift <= {1'b1, data_in, 1'b0};  // LSB first
-                tx_busy <= 1;
-                bit_idx <= 0;
-                baud_counter <= 0;
-              end
-          end 
-        else 
-          begin // Start-block: during transmission of data
-            baud_counter <= baud_counter + 1;
-            if (baud_counter >= BAUD_TICKS) 
-              begin // Start-block: sending each bit during the right period
-                baud_counter <= 0;
-                tx <= tx_shift[bit_idx];
-                bit_idx <= bit_idx + 1;
-
-                if (bit_idx == 9) 
-                  begin // Start-block: announce the send process is done and resets variables to avoid latches
-                    tx <= 1;
-                    tx_busy <= 0; // Done sending all bits
-                    baud_counter <= 0;
+                tx <= 1;
+                tx_busy <= 0; 
+                if (tx_start)
+                  begin
+                    // Load start bit, data bits, and stop bit(s)
+                    tx_shift <= {1'b1, data_in, 1'b0};  // LSB first
+                    tx_busy <= 1;
                     bit_idx <= 0;
-                    tx_shift <= 10'b1111111111;
-                  end // End-block: announce the send process is done and resets variables to avoid latches
-              end // End-block: sending each bit during the right period
-          end // End-block: during transmission of data
+                    baud_counter <= 0;
+                    state <= SEND;
+                  end
+              end
+            SEND:
+              begin
+                if (baud_counter >= BAUD_TICKS) 
+                  begin // Start-block: sending each bit during the right period
+                    baud_counter <= 0;
+                    tx <= tx_shift[bit_idx];
+                    bit_idx <= bit_idx + 1'b1;
+                    if (bit_idx == 9) 
+                        state <= DONE;
+                  end // End-block: sending each bit during the right period
+                else
+                    baud_counter <= baud_counter + 1'b1;
+              end
+            DONE:
+              begin
+                tx <= 1;
+                tx_busy <= 0; // Done sending all bits
+                baud_counter <= 0;
+                bit_idx <= 0;
+                tx_shift <= 10'b1111111111;
+                state <= IDLE;
+              end
+        endcase
       end
 end
 
