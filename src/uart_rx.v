f7b3e5f38816045175ff5af87d7d8b1de0f4a348
dbouche <62671119+dbouche@users.noreply.github.com>
2025-05-14 16:36:24 -0500

    Update uart_rx.v
    added a FSM to the Rx logic and fixed a "truncated variable" warning by adding or substracting the exact amount of bits (i.e., + 1'b1)

--- src/uart_rx.v
+++ src/uart_rx.v
@@ -1,7 +1,7 @@
-
-module uart_rx #(parameter BAUDRATE = 115200, 
-                        CLOCK_FREQ = 27000000,
-                        BAUD_TICKS = CLOCK_FREQ / BAUDRATE)
+//Rx
+module uart_rx #(parameter  BAUDRATE = 115200, 
+                            CLOCK_FREQ = 27000000,
+                            BAUD_TICKS = CLOCK_FREQ / BAUDRATE) 
 (
     input clock,          // clk input
     input n_reset,        // reset input
@@ -9,12 +9,18 @@ module uart_rx #(parameter BAUDRATE = 115200,
     output reg [7:0] data_out,
     output reg rx_ready
 );
-
+ 
 //Rx variables
 reg [15:0] baud_counter; 
 reg [3:0] bit_idx;
 reg [7:0] rx_shift;
-reg receiving;
+
+// State machine
+reg [1:0] state;
+localparam  IDLE = 2'b00,
+            OFFSET = 2'b01, // to offset the bit reading to the middle of the period
+            READ = 2'b10,
+            DONE = 2'b11;
 
 always @(posedge clock or negedge n_reset) 
 begin
@@ -22,41 +28,58 @@ begin
       begin
         data_out <= 8'h00;
         rx_shift <= 8'h00;
-        rx_ready <= 1; // to make the rx_ready as default
+        rx_ready <= 0; // to make the rx_ready as default
         baud_counter <= 0; 
         bit_idx <= 0;
-        receiving <= 0;
+        state <= IDLE;
       end
-    else if (bit_idx == 8) // to tell the top module when it's done with the data
-      begin // Start-Block: Finish processing the received byte, raise the 'done' flag and reset variables to avoid latches
-        bit_idx <= 0;
-        rx_ready <= 1; // Equivalent of a 'Done' flag
-        data_out <= rx_shift;
-        rx_shift <= 8'h00;
-        receiving <= 0; // finish the process of receiving data
-      end // End-Block: Finish processing the received byte, raise the 'done' flag and reset variables to avoid latches
     else 
       begin
-        // Resetting variables to avoid the creation of latches
         rx_ready <= 0;
-        data_out <= 0;
-        // Main code for the appropiate period to process data
-        baud_counter <= baud_counter + 1; // to wait for each bit to be read
-        if (baud_counter >= BAUD_TICKS)
-          begin // Start-Block: receiving each bit during the right period
-            baud_counter <= 0;  // Resets the counter
-            // Interaction with the data to be read:
-            if (!receiving && !rx)
+
+        case (state)
+            IDLE: 
               begin
-                receiving <= 1;
+                data_out <= 8'h00;
+                rx_shift <= 8'h00;
+                if (!rx) 
+                  begin  // Start bit detected
+                    baud_counter <= 0;
+                    state <= OFFSET;
+                  end
               end
-            else if (receiving) 
-              begin // Start-Block: Reading and storing the received data 
-                rx_shift <= {rx, rx_shift[7:1]}; // Shifting the data from MSB to LSB
-                bit_idx <= bit_idx + 1;
-             end // End-Block: Reading and storing the received data 
-          end // End-Block: receiving each bit during the right period    
-      end
+            OFFSET: // to the middle of the reading period
+              begin
+                if (baud_counter >= BAUD_TICKS / 2) // Wait half bit to sample in middle
+                  begin
+                    baud_counter <= 0;
+                    bit_idx <= 0;
+                    state <= READ;
+                  end 
+                else
+                    baud_counter <= baud_counter + 1'b1;
+              end
+            READ: 
+              begin
+                if (baud_counter >= BAUD_TICKS - 1'b1) 
+                  begin
+                    rx_shift <= {rx, rx_shift[7:1]};
+                    bit_idx <= bit_idx + 1'b1;
+                    baud_counter <= 0;
+                    if (bit_idx == 7)
+                        state <= DONE;
+                  end 
+                else
+                    baud_counter <= baud_counter + 1'b1;
+              end
+            DONE: 
+              begin
+                data_out <= rx_shift;
+                rx_ready <= 1;
+                state <= IDLE;
+              end
+        endcase
+    end
 end
 
 endmodule
