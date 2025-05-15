8b7015260a13c4f310b1e4e3a9bb45c9feeb6752
dbouche <62671119+dbouche@users.noreply.github.com>
2025-05-15 09:42:42 -0500

    Update all uart_top.v
    -A FSM was created
    -A new instantitiation module was created (out_trigger) to execute an order received by the host device (i.e., generate a trigger, a visual LED feedback, etc.)
    -A debouncing method for both the Rx pulse and the button press was improved
    -An echo function was added to tell the host it was correctly received by the FPGA
    -Identification for the character received or the echo function to send to the Tx module and the new out_trigger module

--- src/uart_top.v
+++ src/uart_top.v
@@ -1,90 +1,135 @@
-module uart_top #(parameter BAUDRATE = 115200, 
-                            CLOCK_FREQ = 27000000)(
+//Top
+module top #(parameter  BAUDRATE =  115200, 
+                        CLOCK_FREQ = 27000000)
+(
     input sys_clk,          // clk input
     input sys_rst_n,        // reset input
     input rx,
     input button,
     output wire tx,
-    output reg [5:0] led    // 6 LEDS pin 
+    output wire [5:0] led   // 6 LEDS pin 
 );
 
 // Constants to send data
-parameter CHARACTER = 8'h41; // Send any ASCII character (i.e., 0x41 = 'A')
+localparam  CHARACTER = 8'h45, // Send any ASCII character (i.e., 0x45 = 'E')
+            RECEIVED = 8'h52,   // Send back to the device that a data was received successfully (i.e., 0x52 = 'R') 
+            ASCII_A = 8'h41,
+            ASCII_B = 8'h42,
+            ASCII_C = 8'h43,
+            ASCII_D = 8'h44;
 
-// Created variables to connect across submodules
-
-// Variables to connect across submodules: reg (created here) & wire (created inside a submodule)
+// Variables to connect across submodules: reg (created here) & wire (created inside a submodule or outside an always block)
 //	for receiver: uart_rx
 wire [7:0] rx_data;
+reg [7:0] copy_rx_data;
 wire rx_ready;
+reg rx_ready_prev;
+wire rx_ready_pulse;
 //	for transmitter: uart_tx
 reg tx_start;
+reg [7:0] send_data;
 reg [7:0] tx_data;
 wire tx_busy;
-reg tx_ready;
-// variable for the button
-reg hold_button;
 
+// Variable for the button
+reg button_prev;
+wire button_falling_edge;
+
+// Variable for the Spectrometer trigger
+reg new_pattern_in; 
+reg [1:0] pulse_rate;
+
+// State machine
+reg [1:0] state;
+localparam  IDLE = 2'b00,
+            RX_PROCESS = 2'b01,
+            TX_SEND = 2'b10;
+
+// Submodule instantiations
 uart_rx  #(.BAUDRATE(BAUDRATE), .CLOCK_FREQ(CLOCK_FREQ)) 
         rx1 (.clock(sys_clk), .n_reset(sys_rst_n), .rx(rx), .data_out(rx_data), .rx_ready(rx_ready));
 
 uart_tx  #(.BAUDRATE(BAUDRATE), .CLOCK_FREQ(CLOCK_FREQ)) 
         tx1 (.clock(sys_clk), .n_reset(sys_rst_n), .tx_start(tx_start), .data_in(tx_data), .tx(tx), .tx_busy(tx_busy));
 
+out_trigger outt1 (.clock(sys_clk), .n_reset(sys_rst_n), .new_pattern_in(new_pattern_in), .pulse_rate(pulse_rate), .led(led));
 
+// Main code
 always @(posedge sys_clk or negedge sys_rst_n)
 begin
 	if (!sys_rst_n)
-	  begin
-		led <= 6'b111111; // all LEDs are off this way on the Tang Nano
+	  begin  
 		tx_start <= 0; 
 		tx_data <= 8'h00; // to standarize the use of this variable as in ASCII format 
-        hold_button <= 0;
-        tx_ready <= 1;
+        new_pattern_in <= 0;
+        rx_ready_prev <= 0;
+        copy_rx_data <= 0;
+        pulse_rate <= 0;
+        send_data <= 0;
+        button_prev <= 0;
+        state <= IDLE;
 	  end
 	else
 	  begin
-		if (rx_ready)
-		  begin // Start-Block: to prioritize UART reception and avoid conflicts with any transmission
-			tx_start <= 0;
-            if (rx_data != 8'h00)
-			  begin // Start-Block: to check a valid input has been received
-                if (rx_data == 8'h46) // ASCII 'F'
-                  begin
-                    led <= 6'b111110;
-                  end
-                else if (rx_data == 8'h47) // ASCII 'G'
-                  begin
-                    led <= 6'b111100;
-                  end 
-              end // End-Block: to check a valid input has been received
-		  end // End-Block: to prioritize UART reception and avoid conflicts with any transmission
-		else
-		  begin // Start-Block: Tx Logic
-            // Detect falling edge of button (active-low press)
-            if (!button && !hold_button)
-              begin
-                hold_button <= 1;
-                tx_ready <= 1; // tx flag for the sending process
-              end 
-            else if (button && hold_button) 
-              begin
-                hold_button <= 0;  // Resets hold when button is released
-              end
+        // debouncing/sync variables
+        rx_ready_prev <= rx_ready;
+        button_prev <= button;
+        
+        // Deassert single-cycle control signals
+        new_pattern_in <= 0;
+        tx_start <= 0;
 
-            // Start Tx only if the flag is ready, the process is not busy and the button was pressed 
-            if (!tx_busy && tx_ready && hold_button) 
+        case(state)
+            IDLE:
               begin
-                tx_data <= CHARACTER; // this constant can be changed at the CHARACTER parameter 
-                tx_start <= 1;
-                tx_ready <= 0;
-              end 
-            else 
-              begin
-                tx_start <= 0;  // Ensure it's a one-cycle pulse
+                tx_data <= 8'h00;
+                if (rx_ready_pulse) // prioritize the detection of data
+                  begin
+                    tx_start <= 0;
+                    copy_rx_data <= rx_data; // store the data into a local variable
+                    if (rx_data != 8'h00)
+                        state <= RX_PROCESS;
+                  end
+                else if (button_falling_edge && !tx_busy)
+                  begin
+                    send_data <= CHARACTER;
+                    state <= TX_SEND;
+                  end
               end
-		  end // End-Block: Tx Logic		  
+            RX_PROCESS:
+              begin
+                if (copy_rx_data >= ASCII_A && copy_rx_data <= ASCII_D)
+                  begin
+                    new_pattern_in <= 1;
+                    send_data <= RECEIVED;  // This is to acknowledge the byte sent to the FPGA was received (sends back an 'R')
+                    case(copy_rx_data)
+                        ASCII_A: pulse_rate <= 2'b00;
+                        ASCII_B: pulse_rate <= 2'b01;
+                        ASCII_C: pulse_rate <= 2'b10;
+                        ASCII_D: pulse_rate <= 2'b11;
+                    endcase
+                    state <= TX_SEND; 
+                  end
+                else
+                  begin
+                    new_pattern_in <= 0;
+                    send_data <= 0;
+                    state <= IDLE;
+                  end
+                copy_rx_data <= 0; // clear store values befor storing the next one
+              end
+            TX_SEND:
+              begin
+                tx_data <= send_data; // this constant can be changed at the CHARACTER parameter 
+                send_data <= 0;
+                tx_start <= 1;
+                state <= IDLE;
+              end
+        endcase	  
 	  end
 end
+
+assign rx_ready_pulse = rx_ready & ~rx_ready_prev;
+assign button_falling_edge = button_prev & ~button;
 
 endmodule 
